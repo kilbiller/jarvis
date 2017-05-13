b02a7507baa499e2db81fe5c4a32f4ac44267072
Rémy Peru <peru.remy@gmail.com>
2017-05-13 07:36:38 +0200

    Add between rule

---
+++ src/rules/between.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Jarvis\rules;
+
+use function fphp\curry;
+
+/**
+ * Test if values is between minimum and maximum
+ *
+ * @param int $min minimum
+ * @param int $max maximum
+ * @return bool
+ */
+function between($min, $max) {
+	$between = function ($min, $max, $value) {
+		return $min < $value && $value < $max;
+	};
+
+	return curry($between)($min, $max);
+}
---
+++ tests/rules/betweenTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Jarvis\Validator;
+use function Jarvis\rules\between;
+
+describe('between', function () {
+	it('should validate', function () {
+		$array = ['age' => 32];
+
+		$validator = (new Validator())
+		->addRule('age', between(15, 48));
+
+		expect($validator->validate($array))->toBe(true);
+	});
+
+	it('should fail to validate', function () {
+		$array = ['age' => 32];
+
+		$validator = (new Validator())
+		->addRule('age', between(34, 48));
+
+		expect($validator->validate($array))->toBe(false);
+		expect($validator->getErrors())->toBe(['age' => 'age is not valid.']);
+	});
+
+	it('should work by passing a string', function () {
+		$array = ['age' => 32];
+
+		$validator = (new Validator())
+		->addRule('age', \Jarvis\rules\between(15, 48));
+
+		expect($validator->validate($array))->toBe(true);
+	});
+});
